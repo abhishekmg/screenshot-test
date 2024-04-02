0248d92018a196d781ea6e808408de2e4b243a25
abhishekmg <abhishekmg.12@gmail.com>
2024-04-02 12:50:07 +0530

    second commit

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,33 +1,25 @@
-import { launch } from "puppeteer"
-import chromium from "chrome-aws-lambda"
+import { launch } from "puppeteer";
 
 const takeScreenshot = async (url) => {
-
   // this works on my local machine
-	// const browser = await launch();
+  const browser = await launch();
 
-  browser = await chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  });
+  const page = await browser.newPage();
 
-	const page = await browser.newPage();
+  await page.goto(url, { waitUntil: "load" });
 
-	await page.goto(url, { waitUntil: "load" });
+  const buffer = await page.screenshot();
 
-	const buffer = await page.screenshot();
+  await page.close();
+  await browser.close();
 
-	await page.close();
-	await browser.close();
-
-	return buffer;
+  return buffer;
 };
 
 export async function GET(request) {
-  const buffer = await takeScreenshot("https://www.npmjs.com/package/puppeteer")
- 
-  return Response.json({ data: buffer })
+  const buffer = await takeScreenshot(
+    "https://www.npmjs.com/package/puppeteer"
+  );
+
+  return Response.json({ data: buffer });
 }
